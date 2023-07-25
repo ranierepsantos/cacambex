d289783ca4a22762c78e90248cc94de39c1c1286
norgelabs <114440665+norgelabs@users.noreply.github.com>
2023-07-25 10:10:00 -0300

    Alterando url da API

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlApi: "https://cacambex-api-hom-w.azurewebsites.net"
+  urlApi: "https://cacambex-api.azurewebsites.net"
 };
 export const TOKEN_STORAGE = 'TOKEN';
